be8182948e3fc89736a23e374937d61677690ddc
shunchobi <shunbeat1008@gmail.com>
2022-07-07 10:57:27 +0900

    Initial Commit

---
+++ TemotonoOtomo/TemotonoOtomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemotonoOtomoApp.swift
+//  TemotonoOtomo
+//
+//  Created by Shun on 2022/07/07.
+//
+
+import SwiftUI
+
+@main
+struct TemotonoOtomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
